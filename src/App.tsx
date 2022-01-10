1f285ea48e928392079123c6c95e81234b2b5f06
Alexandro <alexandronrossi@hotmail.com>
2022-01-10 11:36:09 -0300

    Implementando e estilizando modal (parte 1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
+
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { Dashboard } from "./components/Dashboard";
 
 import { GlobalStyle } from "./styles/global";
 
+
 export function App() {
+  const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTrasactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTrasactionModalOpen(false);
+    }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      < NewTransactionModal isOpen={isNewTrasactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+
       <GlobalStyle />
     </>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,15 +2,20 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     :root {
+        --purple: #5429CC;
+        --purple-light:#6933FF;
         --red: #E62E4D;
         --blue: #5429CC;
         --blue-light: #6933FF;
         --green: #33CC95;
+        --green-shadow: #12A454;
         
         --text-title: #363F5F;
         --text-body: #969CB3;
         
         --background: #F0F2F5;
+        --background-box:#E7E9EE;
+        --border-box:#D7D7D7;
         --shape: #FFFFFF;
     }
 
@@ -32,7 +37,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        background-color: var(--background);
+        background: var(--background);
         -webkit-font-smoothing: antialiased;
 
     }
@@ -53,6 +58,43 @@ export const GlobalStyle = createGlobalStyle`
     [disabled] {
         opacity: 0.6;
         cursor: not-allowed;
+    }
+
+    .react-modal-overlay {
+        background: rgba(0, 0, 0, 0.5);
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.25rem;
+    }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.35rem;
+        top: 1.25rem;
+        border: 0;
+        background: transparent;
+
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.8)
+        }
     }
 
 `;
