de9a55501eb3fb20b5795b5981e44298d8cdcc55
Alexandro <alexandronrossi@hotmail.com>
2022-01-10 12:54:03 -0300

    Adição e estilização do botão de tipo de transação

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,12 @@
 
 import Modal from 'react-modal';
+import { useState } from 'react';
 
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 Modal.setAppElement('#root');
 
@@ -12,6 +16,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
+
     return (
         <Modal 
             isOpen={isOpen} 
@@ -27,8 +33,31 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <h2>Cadastrar transação</h2>
 
                 <input type="text" placeholder='Título' />
+
                 <input type="number" placeholder='Valor' />
+
+                <TransactionTypeContainer>
+                    <RadioBox 
+                        type="button" 
+                        onClick={() => { setType('deposit') }}
+                        isActive={type === 'deposit'}
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </RadioBox>
+
+                    <RadioBox 
+                        type="button" 
+                        onClick={() => { setType('withdraw') } }
+                        isActive={type === 'withdraw'}
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
+
                 <input type="text" placeholder='Categoria' />
+
                 <button type="submit">Cadastrar</button>
             </Container>
         </Modal>
